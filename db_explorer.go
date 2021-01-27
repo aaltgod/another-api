5ea1756275d08a03d946576a4cc0ea55be780cd8
alaskastorm <montanaof300@mail.ru>
2021-01-27 14:29:28 +0300

    add switch, cases for handlers

--- db_explorer.go
+++ db_explorer.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"log"
 	"net/http"
 )
@@ -10,9 +11,11 @@ type Handler struct {
 	DB *sql.DB
 }
 
+type Response map[string]interface{}
+
 func NewDbExplorer(db *sql.DB) (http.Handler, error) {
 
-	testHandler := &Handler{}
+	testHandler := &Handler{DB: db}
 
 	return testHandler, nil
 }
@@ -20,21 +23,70 @@ func NewDbExplorer(db *sql.DB) (http.Handler, error) {
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL.Path)
 
+	switch r.Method {
+	case "POST":
+		h.Create(w, r)
+	case "GET":
+		h.Read(w, r)
+	case "PUT":
+		h.Update(w, r)
+	case "DELETE":
+		h.Delete(w, r)
+	default:
+		response, _ := json.Marshal(&Response{
+		"error": "unknown table",
+		})
+
+		w.WriteHeader(http.StatusNotFound)
+		w.Write(response)
+	}
 }
 
 func (h *Handler) Create(w http.ResponseWriter, r *http.Request) {
+	log.Println("CREATE:", r.URL.Path)
 
 }
 
 func (h *Handler) Read(w http.ResponseWriter, r *http.Request) {
+	log.Println("READ:", r.URL.Path)
+
+	log.Println(h.DB.Stats())
+
+	_, err := h.DB.Query("SELECT *")
+	if err != nil {
+		log.Println("ROWS:", err)
+		response, _ := json.Marshal(&Response{
+			"error": "internal server error",
+		})
+
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(response)
+
+		return
+	}
+
+	//result, err := h.DB.Exec("SELECT *")
+	//if err != nil {
+	//	log.Println("RESULT:", err)
+	//	return
+	//}
+	//affected, err := result.RowsAffected()
+	//if err != nil {
+	//	log.Println("AFFECTED", err)
+	//	return
+	//}
+	//log.Println(affected)
+
 
 }
 
 func (h *Handler) Update(w http.ResponseWriter, r *http.Request) {
+	log.Println("UPDATE:", r.URL.Path)
 
 }
 
 func (h *Handler) Delete(w http.ResponseWriter, r *http.Request) {
+	log.Println("DELETE:", r.URL.Path)
 
 }
 
